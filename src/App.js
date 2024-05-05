56db5d3f9e4179e9b19073ecca5e7ee26e4c35f4
Anastasiia <nastyadedela@gmail.com>
2024-05-05 16:48:28 +0200

    feat: added gallery functionality

--- src/App.js
+++ src/App.js
@@ -1,49 +1,88 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './index.scss';
-
-function Collection({ name, images }) {
-  return (
-    <div className="collection">
-      <img className="collection__big" src={images[0]} alt="Item" />
-      <div className="collection__bottom">
-        <img className="collection__mini" src={images[1]} alt="Item" />
-        <img className="collection__mini" src={images[2]} alt="Item" />
-        <img className="collection__mini" src={images[3]} alt="Item" />
-      </div>
-      <h4>{name}</h4>
-    </div>
-  );
-}
+import Collection from './components/Collection';
 
 function App() {
+  const [categories, setCategories] = useState([]);
+  const [collections, setCollections] = useState([]);
+  const [searchValue, setSearchValue] = useState('');
+  const [activeCategory, setActiveCategory] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
+  const [page, setPage] = useState(1);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    const category = activeCategory ? `category=${activeCategory}` : '';
+
+    fetch('https://f38de55f56f9987f.mokky.dev/categories')
+      .then((res) => res.json())
+      .then((categories) => setCategories(categories))
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setIsLoading(false));
+
+    fetch(`https://f38de55f56f9987f.mokky.dev/collections?page=${page}&limit=6&${category}`)
+      .then((res) => res.json())
+      .then((collections) => setCollections(collections))
+      .catch((error) => {
+        console.log(error);
+      })
+      .finally(() => setIsLoading(false));
+  }, [activeCategory, page]);
+
+  console.log(collections);
+
+  const onChangeSesrchValue = (event) => {
+    setSearchValue(event.target.value);
+  };
+
   return (
     <div className="App">
       <h1>Моя коллекция фотографий</h1>
       <div className="top">
         <ul className="tags">
-          <li className="active">Все</li>
-          <li>Горы</li>
-          <li>Море</li>
-          <li>Архитектура</li>
-          <li>Города</li>
+          {categories.map((category, index) => (
+            <li
+              key={index}
+              className={activeCategory === index ? 'active' : ''}
+              onClick={() => setActiveCategory(index)}>
+              {category.name}
+            </li>
+          ))}
         </ul>
-        <input className="search-input" placeholder="Поиск по названию" />
-      </div>
-      <div className="content">
-        <Collection
-          name="Путешествие по миру"
-          images={[
-            'https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MTN8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1560840067-ddcaeb7831d2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8NDB8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1531219572328-a0171b4448a3?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mzl8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-            'https://images.unsplash.com/photo-1573108724029-4c46571d6490?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8MzR8fGNpdHl8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60',
-          ]}
+
+        <input
+          value={searchValue}
+          onChange={onChangeSesrchValue}
+          className="search-input"
+          placeholder="Поиск по названию"
         />
       </div>
+      <div className="content">
+        {isLoading ? (
+          <h2>Loading...</h2>
+        ) : (
+          collections.items &&
+          collections.items
+            .filter((collection) =>
+              collection.name.toLowerCase().includes(searchValue.toLowerCase()),
+            )
+            .map((collection, index) => (
+              <Collection key={index} name={collection.name} images={collection.photos} />
+            ))
+        )}
+      </div>
       <ul className="pagination">
-        <li>1</li>
-        <li className="active">2</li>
-        <li>3</li>
+        {[...Array(5)].map((_, index) => (
+          <li
+            key={index}
+            onClick={() => setPage(index + 1)}
+            className={page === index + 1 ? 'active' : ''}>
+            {index + 1}
+          </li>
+        ))}
       </ul>
     </div>
   );
